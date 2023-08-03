fceee1a62002878cad689d0c17288ac99fad1d26
kgoebber <kevin.goebbert@valpo.edu>
2023-08-03 16:49:18 -0500

    add bash script to create and publish full book

---
+++ publish_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# A script to run and publish the full MetPy Book.
+#
+# By: Kevin Goebbert
+#
+
+# Create Full Jupyter Book as HTML
+jupyter-book build --all .
+
+# Publish Webpage via ghp
+ghp-import -n -p -f _build/html
+
+# Create Latex PDF version of book
+jupyter-book build ../metpy_thebook --builder pdflatex
